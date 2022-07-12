8b8dea6e49e7cad580ff844013f3593e91f573c5
Son Pham <thesonpham120499@gmail.com>
2022-07-12 09:19:37 +0700

    bosung btap cau truc vong lap

---
+++ .history/index_20220712091909.js
@@ -0,0 +1,28 @@
+`function cong() {
+    let a = document.getElementById("select1").value;
+    let b = document.getElementById("select2").value;
+    a = parseInt(a);
+    b = parseInt(b);
+    alert(a+b);
+}
+function tru() {
+    let a = document.getElementById("select1").value;
+    let b = document.getElementById("select2").value;
+    a = parseInt(a);
+    b = parseInt(b);
+    alert(a-b);
+}
+function nhan() {
+    let a = document.getElementById("select1").value;
+    let b = document.getElementById("select2").value;
+    a = parseInt(a);
+    b = parseInt(b);
+    alert(a*b);
+}
+function chia() {
+    let a = document.getElementById("select1").value;
+    let b = document.getElementById("select2").value;
+    a = parseInt(a);
+    b = parseInt(b);
+    alert(a/b);
+}
